c5766b699bc814c3657dbe4c5a5af0431bc8ce1e
alejanderl <alejanderl@alejanderl-K52Je.(none)>
2012-10-17 11:14:23 +0200

    mend

--- app/controllers/events_board_controller.rb
+++ app/controllers/events_board_controller.rb
@@ -3,8 +3,12 @@ class EventsBoardController < ApplicationController
   end
 
   def index
-    @events = Event.uniq_list
-    
+    Event.transaction do
+          @events1=Event.uniq_list
+          @events2=Event.where(:serie_id => 0)
+        end
+        @events = (@events1 | @events2)
+        
      respond_to do |format|
         format.html # show.html.erb
         format.json { render json: @room }
