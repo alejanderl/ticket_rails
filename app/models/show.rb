eb29820d9ae95f67c3e4dfb10fbc24fea9f4154d
alejanderl <alejanderl@alejanderl-K52Je.(none)>
2012-10-25 11:48:27 +0200

    Problema de rutas

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,14 @@
 class Show < ActiveRecord::Base
+  
   attr_accessible :content_related, :description, :name , :image_attributes, :image
   has_and_belongs_to_many :films
   has_many :events
   belongs_to :image
   accepts_nested_attributes_for :image
   
-
+  def self.by_city(city)
+   includes(:events => [{:room =>[{ :theater =>:address }] }]).where("events.date >= ?",Time.now)
+  end
 
  def self.all_nested   
    includes(:events => [{:room =>[{ :theater =>:address }] }])
